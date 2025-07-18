9803dac01b9dd6d1771caf5ef8b4fc0d7b48fc6d
전은배 <dmsqo1403@naver.com>
2025-07-18 12:38:26 +0900

    [코딩채점] 중복문자제거
    Update Sol0106.java

--- C0106/Sol0106.java
+++ C0106/Sol0106.java
@@ -22,6 +22,7 @@ public class Sol0106 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
+        
         String answer = "";
 
         for(int i = 0; i < str.length(); i++) {
