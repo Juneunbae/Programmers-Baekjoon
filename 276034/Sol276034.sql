c3d6c705e56190544ddda7090900c80ef4aa8918
전은배 <dmsqo1403@naver.com>
2025-08-25 14:52:31 +0900

    조건에 맞는 개발자 찾기
    Update Sol276034.sql

--- 276034/Sol276034.sql
+++ 276034/Sol276034.sql
@@ -5,20 +5,20 @@
     SKILLCODES 테이블은 개발자들이 사용하는 프로그래밍 언어에 대한 정보를 담은 테이블입니다. 
     SKILLCODES 테이블의 구조는 다음과 같으며, NAME, CATEGORY, CODE는 각각 스킬의 이름, 스킬의 범주, 스킬의 코드를 의미합니다. 
     스킬의 코드는 2진수로 표현했을 때 각 bit로 구분될 수 있도록 2의 제곱수로 구성되어 있습니다.
-    NAME	    TYPE	    UNIQUE	NULLABLE
-    NAME	    VARCHAR(N)	    Y	        N
-    CATEGORY	    VARCHAR(N)	    N	        N
-    CODE	    INTEGER	    Y	        N
+    NAME	    TYPE	     UNIQUE	NULLABLE
+    NAME	    VARCHAR(N)	 Y	    N
+    CATEGORY   VARCHAR(N)	 N	    N
+    CODE	    INTEGER	     Y	    N
 
     DEVELOPERS 테이블은 개발자들의 프로그래밍 스킬 정보를 담은 테이블입니다. 
     DEVELOPERS 테이블의 구조는 다음과 같으며, ID, FIRST_NAME, LAST_NAME, EMAIL, SKILL_CODE는 각각 개발자의 ID, 이름, 성, 이메일, 스킬 코드를 의미합니다. 
     SKILL_CODE 컬럼은 INTEGER 타입이고, 2진수로 표현했을 때 각 bit는 SKILLCODES 테이블의 코드를 의미합니다.
-    NAME	    TYPE	    UNIQUE	NULLABLE
-    ID	            VARCHAR(N)	    Y	        N
-    FIRST_NAME	    VARCHAR(N)	    N	        Y
-    LAST_NAME	    VARCHAR(N)	    N	        Y
-    EMAIL	    VARCHAR(N)	    Y	        N
-    SKILL_CODE	    INTEGER	    N	        N
+    NAME	    TYPE	   UNIQUE	NULLABLE
+    ID	        VARCHAR(N)	Y	    N
+    FIRST_NAME	VARCHAR(N)	N	    Y
+    LAST_NAME	VARCHAR(N)  N	    Y
+    EMAIL	    VARCHAR(N)	Y       N
+    SKILL_CODE	INTEGER	    N	    N
 
     예를 들어 어떤 개발자의 SKILL_CODE가 400 (=b'110010000')이라면, 이는 SKILLCODES 테이블에서 CODE가 256 (=b'100000000'), 128 (=b'10000000'), 16 (=b'10000') 에 해당하는 스킬을 가졌다는 것을 의미합니다.
 
